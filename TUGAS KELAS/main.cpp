ee500ae89a35c4a55c9d279c0a8229f86b791f11
Kaila <kailatalitha142@gmail.com>
2025-04-13 08:08:53 +0700

    Update main.cpp

--- TUGAS KELAS/main.cpp
+++ TUGAS KELAS/main.cpp
@@ -546,7 +546,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("Bunga Biru dengan Daun Oval");
+    glutCreateWindow("Tugas Proyeksi Bunga 3D");
     
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
